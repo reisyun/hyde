346355c286449fa43b7fa98320d715b9ae1bf003
reisyun <reisyun7@gmail.com>
2022-06-28 02:15:49 +0900

    fix: Use getx to get history data

--- lib/controllers/history_controller.dart
+++ lib/controllers/history_controller.dart
@@ -1,8 +1,22 @@
 import 'package:get/get.dart';
 import 'package:hyde/models/comment.dart';
+import 'package:hyde/utils/read_json.dart';
 
 class HistoryController extends GetxController {
   final histories = const <Comment>[].obs;
+
+  @override
+  void onInit() async {
+    const userId = 'u1';
+
+    final data = await readJson('assets/data/comment.json');
+    final find = data.where((i) => i['author']['id'] == userId);
+    final comments = find.map((c) => Comment.fromJson(c));
+
+    histories.addAll(comments);
+
+    super.onInit();
+  }
 
   void add(Comment comment) {
     // 동일한 미디어가 있는 지 확인
